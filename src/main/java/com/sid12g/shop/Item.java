622f20586159d8f902c1b5388056fe4457f922c5
SID12g <sid12g@naver.com>
2024-03-17 01:33:58 +0900

    feat: lombok

--- src/main/java/com/sid12g/shop/Item.java
+++ src/main/java/com/sid12g/shop/Item.java
@@ -1,14 +1,18 @@
 package com.sid12g.shop;
 
 import jakarta.persistence.*;
+import lombok.ToString;
 
 @Entity
+@ToString
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // id가 1씩 자동으로 증가
     public Long id;
 
     public String title;
     public Integer price;
+
+
 }
 
 
--- src/main/java/com/sid12g/shop/ItemController.java
+++ src/main/java/com/sid12g/shop/ItemController.java
@@ -1,16 +1,28 @@
 package com.sid12g.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
 
     @GetMapping("/list")
     String list(Model model){
-        model.addAttribute("name", "비싼 바지");
+        List<Item> result = itemRepository.findAll();
+//        System.out.println(result.get(0).price);
+        model.addAttribute("items", result);
+
+        var a = new Item();
+        System.out.println(a.toString());
         return "list.html";
     }
 
